792c3bc4c91bd0a97654adf5cdfc8ee1c63a9755
Erika Torres Figueroa <torrespurple@gmail.com>
2023-01-24 14:48:25 -0500

    Added functions for sign in and sign up

--- PawsPlaydate/ContentView.swift
+++ PawsPlaydate/ContentView.swift
@@ -6,6 +6,35 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+
+class AppViewModel: ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email,
+                    password: password) { result,
+            error in
+            guard result != nil, error == nil else {
+                return
+            }
+        }
+    }
+    
+    func sigup(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+        }
+    }
+    
+}
 
 struct ContentView: View {
     @State var email = ""
