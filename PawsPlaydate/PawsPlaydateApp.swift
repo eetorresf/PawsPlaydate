38bcd452fec3d56e991822c0a5d68b878e8cf556
Erika Torres Figueroa <torrespurple@gmail.com>
2023-01-24 13:58:57 -0500

    Initial Commit

---
+++ PawsPlaydate/PawsPlaydateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsPlaydateApp.swift
+//  PawsPlaydate
+//
+//  Created by Erika Dey on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PawsPlaydateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
